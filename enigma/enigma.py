118e4f3f8b5a45ba7440c2da0614a225970dcb05
Jack Kang <dongwon900@gmail.com>
2022-07-04 10:41:07 +0900

    created starter code for linkedlist

---
+++ enigma/enigma.py
@@ -0,0 +1,14 @@
+# enigma project -- we will write a python implementation of the enigma machine
+
+# enigma was a cipher used and developed by German forces in WW2.
+# the machine consists of plugboards and electrical rotors used for physical encryption.
+
+class enigma:
+    pass
+
+class rotor:
+    pass
+
+class plugboard:
+    pass
+
